70670aa316266bdad61d683705a4b04b6bd1bbea
aungmyo-myint <aungmyomyint0412@gmail.com>
2023-02-13 14:58:49 +0630

    update config

--- services/dbConnection.js
+++ services/dbConnection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 
 const dbConnection = mysql.createConnection({
- host: "localhost" || process.env.CLEARDB_DATABASE_URL,
+ host: "localhost" || process.env.host,
  user: "root" || process.env.db_name,
  password: "" || process.env.db_pswd,
- database: "testapp",
+ database: "testapp" || process.env.db,
 });
 
 
