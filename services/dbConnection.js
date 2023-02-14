66ca082b4f79aafafa3659350503a6e1ab2e227a
aungmyo-myint <aungmyomyint0412@gmail.com>
2023-02-14 13:54:50 +0630

    db conn update

--- services/dbConnection.js
+++ services/dbConnection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 
 const dbConnection = mysql.createConnection({
- host: "localhost" || process.env.host,
- user: "root" || process.env.db_name,
- password: "" || process.env.db_pswd,
- database: "testapp" || process.env.db,
+ host: "localhost" || process.env.ps_host,
+ user: "root" || process.env.ps_name,
+ password: "" || process.env.ps_pswd,
+ database: "testapp" || process.env.ps_db,
 });
 
 dbConnection.connect(err=> console.log(err) );
