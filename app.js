6739ac2d63ddd6de5845df0ca9a8159e97aa58d0
aungmyo-myint <aungmyomyint0412@gmail.com>
2023-02-14 11:10:45 +0630

    add env port

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.all("*", (req, res, next) => {
 });
 
 
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT ;
 app.listen(PORT, () => {
  console.log(`server running on port ${PORT}`);
 });
