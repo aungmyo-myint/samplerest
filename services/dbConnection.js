daf4ae2e59f1bcae069afb1bf123995809cac567
aungmyo-myint <aungmyomyint0412@gmail.com>
2023-02-14 14:14:42 +0630

    db connection update

--- services/dbConnection.js
+++ services/dbConnection.js
@@ -1,14 +1,16 @@
 const mysql = require('mysql2');
 
+const dbConnection = mysql.createConnection(process.env.DATABASE_URL)
+console.log('Connected to PlanetScale!')
 
-const dbConnection = mysql.createConnection({
- host: "localhost" || process.env.ps_host,
- user: "root" || process.env.ps_name,
- password: "" || process.env.ps_pswd,
- database: "testapp" || process.env.ps_db,
- ssl: {}
-});
+// // const dbConnection = mysql.createConnection({
+// //  host: "localhost" || process.env.ps_host,
+// //  user: "root" || process.env.ps_name,
+// //  password: "" || process.env.ps_pswd,
+// //  database: "testapp" || process.env.ps_db,
+// //  ssl: {}
+// // });
 
-dbConnection.connect(err=> console.log(err) );
+// dbConnection.connect(err=> console.log(err) );
 
 module.exports = dbConnection;
