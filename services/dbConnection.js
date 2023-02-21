908bb5cd7484d772143b3c9b55c12d18f6ae3393
aungmyo-myint <aungmyomyint0412@gmail.com>
2023-02-21 14:37:30 +0630

    nothing

--- services/dbConnection.js
+++ services/dbConnection.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
 
 const dev_dbURL = 'mysql://root:@localhost:3306/testapp'
-const dbConnection = mysql.createConnection(process.env.DATABASE_URL)
+
+const dbConnection = mysql.createConnection(process.env.DATABASE_URL || dev_dbURL)
 console.log('Connected to db!')
 
 dbConnection.connect(err => {  if(err) console.log(err) } );
