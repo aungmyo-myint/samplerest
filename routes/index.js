fc1bbf6b01d595a034514675c55b12a23b49d2d3
aungmyo-myint <aungmyomyint0412@gmail.com>
2023-03-29 11:37:55 +0630

    debug

--- routes/index.js
+++ routes/index.js
@@ -15,11 +15,19 @@ router
  .put(controllers.uploadData);
 
  
+router.route("/skylark-m3s/api/groupLoans.m3s").get(controllers.downloadData).post(controllers.uploadData);
 router.route("/skylark-m3s/api/groupLoan.m3s").get(controllers.downloadData).post(controllers.uploadData);
+
+router.route("/skylark-m3s/api/individualLoans.m3s").get(controllers.downloadData).post(controllers.uploadData);
 router.route("/skylark-m3s/api/individualLoan.m3s").get(controllers.downloadData).post(controllers.uploadData);
+
+router.route("/skylark-m3s/api/surveyResults.m3s").get(controllers.downloadData).post(controllers.uploadData);
 router.route("/skylark-m3s/api/surveyResult.m3s").get(controllers.downloadData).post(controllers.uploadData);
+
 router.route("/skylark-m3s/api/customers.m3s").get(controllers.downloadData).post(controllers.uploadData);
+
 router.route("/skylark-m3s/file/upload.m3s").get(controllers.downloadData).post(controllers.uploadData);
+router.route("/skylark-m3s/file/uploads.m3s").get(controllers.downloadData).post(controllers.uploadData);
 
  router.route("/api/txn/drop").post(controllers.deleteData);
  router
